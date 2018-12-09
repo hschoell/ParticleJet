9625f21b5a70412ab1b2170bc516bf2b489bfe1e
tossas <45742467+tossas@users.noreply.github.com>
2018-12-09 18:08:17 -0500

    Add files via upload

---
+++ eq17.m
@@ -0,0 +1,34 @@
+% Function to calculate del(del p) using 
+function [A_ddp, rhs] = eq17(uStar,vStar,u,v,dx,dy,dt,nx,ny);
+
+% Number of unknowns
+args = length(u(1,:))*length(u(:,1));
+
+% Initializing the RHS ignoring the boundaries. 
+rhs = zeros(args,1);
+
+for j = 2:length(v(1,:))-1 %Iterating over y
+    for i = 2:length(u(:,1))-1 %Iterating over y
+        rhs(i+(j-1)*length(u(:,1))) = 1/dt.*((uStar(i+1,j)-uStar(i,j))/dx...
+            +(vStar(i,j+1)-vStar(i,j))/dy);       
+    end
+end
+
+aw = 1/dx.^2*ones(args,1);
+ae = 1/dx.^2*ones(args,1);
+an = 1/dx.^2*ones(args,1);
+as = 1/dx.^2*ones(args,1);
+ap = -4/dx.^2*ones(args,1);
+
+A = diag(ap)+diag(an(1+nx/2:end-nx/2),nx)+diag(as(1+nx/2:end-nx/2),-nx) ...
+    +diag(ae(1:end-1),1)+diag(aw(1:end-1),-1);
+A_ddp = sparse(A);
+
+end
+
+
+
+
+
+
+
