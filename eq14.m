bc56ee6207e176e24ce3f9f9a884910c8afcfe14
hschoell <45742470+hschoell@users.noreply.github.com>
2018-12-13 20:48:32 -0500

    Update eq14.m

--- eq14.m
+++ eq14.m
@@ -1,27 +1,35 @@
 function duStarStar = eq14(H_0, H_1, u1, d1, d2, dt, Re)
 % This function calculates delta u star star using eqn 14. 
-
+   
 % Determine Number of unknowns
-args = length(u1(1,:))*length(u1(:,1));
 nx = length(u1(:,1));
+ny = length(u1(1,:));
+args = nx*ny;
 
 % Set up the right-hand side. Again; ignore the boundaries; lexiographic
 % ordering
 rhs = zeros(args,1);
-for j = 2:length(u1(1,:))-1 % Iterate over y
-    for i = 2:length(u1(:,1))-1 %Iterate over x
-        rhs(i+(j-1)*length(u1(:,1))) = dt/2*(3*H_1(i,j) - H_0(i,j))...
+for j = 2:ny-1 % Iterate over y
+    for i = 2:nx-1 %Iterate over x
+        rhs(i+(j-1)*nx) = dt/2*(3*H_1(i,j) - H_0(i,j))...
             +dt/(Re)*(1/d1^2*(u1(i+1,j)-2*u1(i,j) + u1(i-1,j))...
             +1/d2^2*(u1(i,j+1)-2*u1(i,j)+u1(i,j-1)));
     end
 end
 
-    for k = 1:129
-        for l = 1:64
-            b = k+129*(l-1);
-            rhs1(k,l) = rhs(b);
-        end
+% Care for outlet boundary conditions
+for j = 2:ny-1
+    rhs(nx+(j-1)*nx) = dt/2*(3*H_1(end,j) - H_0(end,j))...
+            +dt/Re*(1/d1^2*(-u1(i,j) + u1(i-1,j))...
+            +1/d2^2*(-u1(i,j)+u1(i,j-1)));
+end
+
+for k = 1:129
+    for l = 1:64
+        b = k+129*(l-1);
+        rhs1(k,l) = rhs(b);
     end
+end
 
 % Set up the operator on the left-hand-side
 aw = -dt/(2*d1^2*Re)*ones(args,1);
@@ -32,8 +40,8 @@ ae = -dt/(2*d1^2*Re)*ones(args,1);
 ap(2*nx:nx:end-nx) = 1+dt/(d1^2*Re);
 
 % Solve using Thomas-Algorithm
-duStarStar_vec = thomas(aw,ap,ae,rhs,args,nx);   
+duStarStar_vec = thomas(aw,ap,ae,rhs,args);
 
 % Put back into matrix form
-duStarStar = reshape(duStarStar_vec,[length(u1(:,1)),length(u1(1,:))]);
+duStarStar = reshape(duStarStar_vec,[nx,ny]);
 end
