b85ff86ac37a116a939a21b7c99cbe94a213e50b
tossas <45742467+tossas@users.noreply.github.com>
2018-12-13 20:18:45 -0500

    Add files via upload

--- thomas.m
+++ thomas.m
@@ -1,4 +1,4 @@
-function y = thomas(aw,ap,ae,b,args)
+function y = thomas(aw,ap,ae,b,args,nx)
 % Solve a tridiagonal linear system using the Thomas Algorithm.
 
 v = zeros(args,1);
@@ -6,9 +6,13 @@ y = v;
 w = ap(1);
 y(1) = b(1)/w;
 for i = 2:args
-    v(i-1) = ae(i-1)/w;
+    if mod(i,nx)==1
+        v(i-1) = 0;
+    else
+        v(i-1) = ae(i-1)/w;
+    end
     w = ap(i) - aw(i)*v(i-1);
-    y(i) = (b(i) - aw(i)*y(i-1))/w;
+    y(i) = (b(i) - aw(i)*y(i-1))/w;         
 end
 for j = args-1:-1:1
     y(j) = y(j) - v(j)*y(j+1);
